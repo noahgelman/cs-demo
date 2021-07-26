3c8c930e922a6d0a859d8cb3d15a391f20979b64
noahgelman <noahmgelman@gmail.com>
2021-07-26 00:35:32 -0700

    HTML/CSS update. Starting functionality

--- app/components/file-table.js
+++ app/components/file-table.js
@@ -1,3 +1,13 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
-export default class FileTableComponent extends Component {}
+export default class FileTableComponent extends Component {
+  @tracked selectedCount = 0;
+
+
+  @action
+  updateSelectedCount() {
+    this.selectedCount = this.selectedCount + 1;
+  }
+}
