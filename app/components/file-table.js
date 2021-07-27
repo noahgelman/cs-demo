68456257094c53918f16d34c291c43f3a7e0f21e
noahgelman <noahmgelman@gmail.com>
2021-07-26 17:20:00 -0700

    Finalized HTML/CSS

--- app/components/file-table.js
+++ app/components/file-table.js
@@ -3,11 +3,79 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class FileTableComponent extends Component {
-  @tracked selectedCount = 0;
+  @tracked allSelected = false;
+  @tracked checkboxes = {};
+  indeterminateCheckbox;
 
+  get selectedCount() {
+    let selectedCount = 0;
+    for (const property in this.checkboxes) {
+      if (this.checkboxes[property].checked) selectedCount++;
+    }
+    return selectedCount;
+  }
+
+  get availableFilesLength() {
+    return Object.keys(this.checkboxes).length;
+  }
+
+  constructor() {
+    super(...arguments);
+
+    this.args.files.forEach((value, index) => {
+      if (value.status == 'available') {
+        this.checkboxes[`file${ index }`] = {
+          checked: false,
+          path: value.path
+        };
+      }
+    });
+  }
 
   @action
-  updateSelectedCount() {
-    this.selectedCount = this.selectedCount + 1;
+  setIndeterminateCheckbox(element) {
+    this.indeterminateCheckbox = element;
+  }
+
+  @action
+  updateIndeterminateCheckbox(index, event) {
+    this.checkboxes[`file${ index }`].checked = event.target.checked;
+    let checkedCount = 0;
+    for (const property in this.checkboxes) {
+      if (this.checkboxes[property].checked) checkedCount++;
+    }
+
+    if (checkedCount === 0) {
+      this.allSelected = false;
+      this.indeterminateCheckbox.indeterminate = false;
+    } else if (checkedCount === this.availableFilesLength) {
+      this.allSelected = true;
+      this.indeterminateCheckbox.indeterminate = false;
+    } else {
+      this.allSelected = false;
+      this.indeterminateCheckbox.indeterminate = true;
+    }
+
+    this.checkboxes = this.checkboxes;
+  }
+
+  @action
+  updateSelectAll(event) {
+    for (const property in this.checkboxes) {
+      this.checkboxes[property].checked = event.target.checked;
+    }
+
+    this.checkboxes = this.checkboxes;
+  }
+
+  @action
+  alertFilePaths(event) {
+    let alertString = '';
+    for (const property in this.checkboxes) {
+      if (this.checkboxes[property].checked) {
+        alertString += `${ this.checkboxes[property].path }\n`;
+      }
+    }
+    alert(alertString);
   }
 }
